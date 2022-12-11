d80340b8de719fe81e8d43cd1e53dbdce8afa901
Obada <obada8411@gmail.com>
2022-12-11 11:33:54 +0200

    Add handle temperature method when update device data

--- routes/v2/devices.js
+++ routes/v2/devices.js
@@ -7,7 +7,15 @@ import {
 
 const devicesRoutes = express.Router();
 
+/* NOTE:
+  when the patient does not use the sensors on the device
+  spo2 will be -999
+  heartRate will be -999
+  temperature will be 0
+*/
+// Update device data on the database and send it to client
 devicesRoutes.put("/devices/update", async (request, response) => {
+  const DISABLED_STATE = -999;
   const device = request.body;
 
   const isDeviceExist = await checkDeviceValidation(device.deviceId);
@@ -18,6 +26,8 @@ devicesRoutes.put("/devices/update", async (request, response) => {
     return;
   }
 
+  handleTemperature();
+  console.log("DEVICE AFTER HANDLE TEMPERATURE", device);
   const { isSuccess, errMessage } = await updateDevice(device);
 
   if (isSuccess) successed();
@@ -72,6 +82,26 @@ devicesRoutes.put("/devices/update", async (request, response) => {
     if (spo2 || heartRate || temperature) return true;
 
     return false;
+  }
+
+  /*
+   * It should change temperature to -999(DISABLED_STATE) if it equal 0
+   * it should change temperature to fixed, for example: 37.257 will change to 37.25
+   */
+  function handleTemperature() {
+    changeTemperatureIfEqualZero();
+    changeTemperatureToFixed(2);
+  }
+
+  function changeTemperatureIfEqualZero() {
+    if (device.dataToUpdate.temperature === 0)
+      device.dataToUpdate.temperature = DISABLED_STATE;
+  }
+
+  function changeTemperatureToFixed(value) {
+    device.dataToUpdate.temperature = Number(
+      device.dataToUpdate.temperature.toFixed(value)
+    );
   }
 });
 
