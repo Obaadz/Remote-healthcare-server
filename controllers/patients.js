5f4958e6be525459cec30addcadb104cc69343d0
Obada <obada8411@gmail.com>
2022-12-13 19:02:17 +0200

    Add 'isRequestedAlready' when returning filtered search results

--- controllers/patients.js
+++ controllers/patients.js
@@ -42,16 +42,15 @@ export const getPatient = async (patientData) => {
 };
 
 export const searchPatientsByDeviceId = async (deviceId) => {
-  const patientDevice = await Devices.find({
+  const patientsDevices = await Devices.find({
     deviceId: deviceId ? new RegExp(`^${deviceId}`) : deviceId,
   });
 
   const [isSuccess, errMessage, data] = await Patients.find({
-    device: patientDevice._id,
+    device: patientsDevices,
   })
-    .select("-password -__v")
+    .select("-password -__v -doctorsRequests")
     .populate("device", "-_id deviceId")
-    .populate("doctorsRequests", "-_id email")
     .then((patients) => [true, "", { patients }])
     .catch((err) => [false, err.message, { patients: [] }]);
 
@@ -59,6 +58,7 @@ export const searchPatientsByDeviceId = async (deviceId) => {
 };
 
 // It return array of patients who don't already added by this doctor email
+// It also add 'IsRequestedAlready' boolean variable, to patients who already has been requested by this doctor email
 export const filterPatientsAlreadyAddedByDoctorEmail = async (patients, doctorEmail) => {
   const doctorPatientsObjectIds = (await Doctors.findOne({ email: doctorEmail }))
     ?.patients;
@@ -72,7 +72,21 @@ export const filterPatientsAlreadyAddedByDoctorEmail = async (patients, doctorEm
     .select("-password -__v")
     .populate("device", "-_id deviceId")
     .populate("doctorsRequests", "-_id email")
-    .then((filterdPatients) => [true, "", { patients: filterdPatients }])
+    .lean()
+    .then((filterdPatients) => {
+      const filteredPatientsWithIsRequestedAlready = filterdPatients.map((patient) => {
+        patient.isRequestedAlready = patient.doctorsRequests.some(
+          (doctor) => doctor.email == doctorEmail
+        );
+
+        // remove the doctors requests (emails) so it will not be send with response...
+        delete patient.doctorsRequests;
+
+        return patient;
+      });
+
+      return [true, "", { patients: filteredPatientsWithIsRequestedAlready }];
+    })
     .catch((err) => [false, err.message, { patients: [] }]);
 
   return { isSuccess, errMessage, data };
