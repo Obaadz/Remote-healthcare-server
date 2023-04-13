9109b9e2e6b5b0500f54f093e12cc74379b0ab92
Obada <obada8411@gmail.com>
2023-04-13 21:58:45 +0200

    Fix issues

--- routes/v2/devices.js
+++ routes/v2/devices.js
@@ -9,23 +9,28 @@ import OneSignal from "@onesignal/node-onesignal";
 
 const devicesRoutes = express.Router();
 
-const configuration = OneSignal.createConfiguration({
+// const configuration = OneSignal.createConfiguration({
+//   userKey: "ZjQxMWY4OWQtNGM0NC00MWJjLWI1YjgtODgxM2YxOTY3YTRm",
+//   appKey: "YzUzZGFjYzEtOTk2My00ZWMxLWEwZDItOTNmY2ZlOGU3MWI3",
+// });
+
+let configuration = OneSignal.createConfiguration({
   userKey: "ZjQxMWY4OWQtNGM0NC00MWJjLWI1YjgtODgxM2YxOTY3YTRm",
   appKey: "YzUzZGFjYzEtOTk2My00ZWMxLWEwZDItOTNmY2ZlOGU3MWI3",
 });
 
-const OneSignalClient = new OneSignal.DefaultApi(configuration);
+const client = new OneSignal.DefaultApi(configuration);
 
 // Update device data on the database and send it to client
 devicesRoutes.put("/devices/update", async (request, response) => {
   const device = request.body;
-  const notifaction = new OneSignal.Notification();
+  // const notifaction = new OneSignal.Notification();
 
-  notifaction.app_id = "fe711cfd-661b-452b-9d63-9e9d4cf56e44";
-  notifaction.contents = { en: "Your device has been updated ya wala" };
-  notifaction.headings = { en: "Device Updated ya wala" };
+  // notifaction.app_id = "fe711cfd-661b-452b-9d63-9e9d4cf56e44";
+  // notifaction.contents = { en: "Your device has been updated ya wala" };
+  // notifaction.headings = { en: "Device Updated ya wala" };
 
-  await OneSignalClient.createNotification(notifaction);
+  console.log(await client.getApps());
 
   const oldDeviceData = await getDeviceData(device.deviceId);
   const isDataToUpdateExist = handleDataToUpdate(device.dataToUpdate, oldDeviceData);
