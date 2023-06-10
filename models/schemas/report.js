5ccdb30bdcbe6ae9225f8b1a6e47702237a2b9b4
Obada <obada8411@gmail.com>
2023-06-10 15:13:51 +0300

    Change temperature type in report schema

--- models/schemas/report.js
+++ models/schemas/report.js
@@ -3,7 +3,7 @@ import mongoose, { Types } from "mongoose";
 export const reportSchema = new mongoose.Schema({
   spo2: Number,
   heartRate: Number,
-  temperature: Number,
+  temperature: String,
   createdAt: Date,
 });
 
