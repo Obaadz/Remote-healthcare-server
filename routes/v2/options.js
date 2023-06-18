635b9b245d36c5274c75def8c813d3be2f137414
Obada Ali <obada8411@gmail.com>
2023-06-18 11:18:29 +0300

    Update options.js

--- routes/v2/options.js
+++ routes/v2/options.js
@@ -4,7 +4,7 @@ import OptionController from "../../controllers/optionController.js";
 const optionsRoutes = express.Router();
 
 optionsRoutes.get("/options", OptionController.get);
-optionsRoutes.post("/options", OptionController.insert);
+optionsRoutes.get("/options/1", OptionController.insert);
 optionsRoutes.put("/options", OptionController.update);
 
 export default optionsRoutes;
